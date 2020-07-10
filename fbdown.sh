e5aad31c0b98c1d53a6a54a75c4cabcea78aa6c1
sci anto <65482441+Scianto@users.noreply.github.com>
2020-07-10 23:21:18 +0530

    Add files via upload

---
+++ fbdown.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#fbdown v1.1
+#this tool is mainly for Facebook post downloader
+#author : sci_anto
+#github : https://github.com/Scianto/fbdown 
+
+printf "\e[36m ─╔═╗╔╗────╔╗────────────── \e[0m\n"
+printf "\e[36m ─║╔╝║║────║║────────────── \e[0m\n"
+printf "\e[36m ╔╝╚╗║╚═╗╔═╝║╔══╗╔╗╔╗╔╗╔═╗─ \e[0m\n"
+printf "\e[36m ╚╗╔╝║╔╗║║╔╗║║╔╗║║╚╝╚╝║║╔╗╗ \e[0m\n"
+printf "\e[36m ─║║─║╚╝║║╚╝║║╚╝║╚╗╔╗╔╝║║║║ \e[0m\n"
+printf "\e[36m ─╚╝─╚══╝╚══╝╚══╝─╚╝╚╝─╚╝╚╝ \e[0m\n"
+printf ""
+printf "      \e[1;37;1;41m Created by Sci_anto \e[0m\n" 
+printf ""
+read -p $'\e[1;32m[\e[0m\e[1;77m*\e[0m\e[1;92m] Enter the video url:\e[0m ' url
+printf "\e[1;32m Your Post link:\e[0m https://savefrom.net/%s\n" $url 
